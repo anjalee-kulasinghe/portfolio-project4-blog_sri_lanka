50ba834eefd49e86e6bb801aa9768eff6d8cea69
Anjalee Kulasinghe <anjaleek@gmail.com>
2024-04-01 10:16:52 +0000

    connect the contact us with the database

--- contact/forms.py
+++ contact/forms.py
@@ -1,7 +1,11 @@
 from django import forms  # Importing forms module from Django
 
 # Defining a contact form
-class ContactForm(forms.Form):
-    name = forms.CharField(label='Your Name', max_length=100)  # Name field
-    email = forms.EmailField(label='Your Email')  # Email field
-    message = forms.CharField(label='Your Message', widget=forms.Textarea)  # Message field with Textarea widget
+from django import forms
+from .models import Contact
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = ['name', 'email', 'message']
+
--- contact/views.py
+++ contact/views.py
@@ -1,24 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from .forms import ContactForm
+from .models import Contact
 
+from django.shortcuts import render, redirect
+from django.views import View
+from .forms import ContactForm
+from .models import Contact
 
 class ContactView(View):
-    # Handling GET requests to display the concat form
     def get(self, request):
-        # Instantiate the UserRegisterForm
         form = ContactForm()
-        # Render the registration form template with the form object
         return render(request, 'contact/contact.html', {'form': form})
 
-    # Handling POST requests to process form submission
     def post(self, request):
-        # Instantiate the ContactForm with the form data from the POST request
         form = ContactForm(request.POST)
-
-        # Check if the form data is valid
         if form.is_valid():
+            form.save()  # Save the form data to the database
             return render(request, 'contact/success.html')
         else:
-            # If form data is invalid, re-render the contact form template with the form and errors
             return render(request, 'contact/contact.html', {'form': form})
+
